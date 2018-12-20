8e9208a193d62d3c48a5ef1ce836cc4842b88c05
ChaCha4 <1062768094@qq.com>
2018-12-20 14:42:52 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+// 开发环境：把所有的东西放在服务器上，只能看不能拿
+// 将项目打包 npm run build
+export default App;
