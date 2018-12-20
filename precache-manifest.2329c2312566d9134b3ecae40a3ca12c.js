b09fbdb1bc751e7966d61d4ddfdbef130b2e98e1
ChaCha4 <1062768094@qq.com>
2018-12-20 14:59:38 +0800

    Updates

---
+++ precache-manifest.2329c2312566d9134b3ecae40a3ca12c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc79eeba74d152678862",
+    "url": "/react-build/static/js/main.bc79eeba.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-build/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "346190b56305f5cddf8c",
+    "url": "/react-build/static/js/runtime~main.346190b5.js"
+  },
+  {
+    "revision": "96f0df1446e23afac19927f9bcc8273a",
+    "url": "/react-build/index.html"
+  }
+];
